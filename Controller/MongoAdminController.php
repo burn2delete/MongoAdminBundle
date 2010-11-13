91431a652e1ef9b7af23e1caa6d3b5e4f7a58817
Steven Surowiec <steven.surowiec@gmail.com>
2010-11-13 20:44:28 +0000

    Setup controller to use twig templates

--- Controller/MongoAdminController.php
+++ Controller/MongoAdminController.php
@@ -4,17 +4,20 @@ namespace Bundle\MongoAdminBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating\Engine;
 
 class MongoAdminController {
 
 	protected $request;
+	protected $templating;
 
-	public function __construct(Request $request) {
+	public function __construct(Request $request, Engine $templating) {
 		$this->request = $request;
+		$this->templating = $templating;
 	}
 
 	public function index() {
-		$content = 'test';
+		$content = $this->templating->render('MongoAdminBundle:Index:index.twig');
 
 		return new Response($content, 200);
 	}
