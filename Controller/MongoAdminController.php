4f8857792bbf2c08f2f54064c08f5847e58dbbb1
Steven Surowiec <steven.surowiec@gmail.com>
2010-11-16 03:26:24 +0000

    Pass document manager to the controller and use it to get a sample collection list

--- Controller/MongoAdminController.php
+++ Controller/MongoAdminController.php
@@ -5,20 +5,32 @@ namespace Bundle\MongoAdminBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Templating\Engine;
+use Doctrine\ODM\MongoDB\DocumentManager;
 
 class MongoAdminController {
 
 	protected $request;
 	protected $templating;
+	protected $documentManager;
 
-	public function __construct(Request $request, Engine $templating) {
+	public function __construct(Request $request, Engine $templating, DocumentManager $documentManager) {
 		$this->request = $request;
 		$this->templating = $templating;
+		$this->documentManager = $documentManager;
 	}
 
 	public function index() {
-		$content = $this->templating->render('MongoAdminBundle:Index:index.twig');
+		$metadataFactory = $this->documentManager->getMetadataFactory();
+		$metadata = $metadataFactory->getAllMetadata();
 
+		$collections = array();
+		foreach ($metadata as $collection) {
+			if (!in_array($collection->getCollection(), $collections)) {
+				$collections[] = $collection->getCollection();
+			}
+		}
+
+		$content = $this->templating->render('MongoAdminBundle:Index:index.twig', array('collections' => $collections));
 		return new Response($content, 200);
 	}
 }
