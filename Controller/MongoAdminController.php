620f6c079355bc45aafa5d1e4fc99981af4e7834
Steven Surowiec <steven.surowiec@gmail.com>
2010-11-20 13:11:56 +0000

    Switched tabs to spaces

--- Controller/MongoAdminController.php
+++ Controller/MongoAdminController.php
@@ -9,20 +9,20 @@ use Bundle\MongoAdminBundle\MongoManager;
 
 class MongoAdminController {
 
-	protected $request;
-	protected $templating;
-	protected $mongoManager;
+    protected $request;
+    protected $templating;
+    protected $mongoManager;
 
-	public function __construct(Request $request, Engine $templating, MongoManager $mongoManager) {
-		$this->request = $request;
-		$this->templating = $templating;
-		$this->mongoManager = $mongoManager;
-	}
+    public function __construct(Request $request, Engine $templating, MongoManager $mongoManager) {
+        $this->request = $request;
+        $this->templating = $templating;
+        $this->mongoManager = $mongoManager;
+    }
 
-	public function index() {
-		$collections = $this->mongoManager->getCollectionsArray();
+    public function index() {
+        $collections = $this->mongoManager->getCollectionsArray();
 
-		$content = $this->templating->render('MongoAdminBundle:Index:index.twig', array('collections' => $collections));
-		return new Response($content, 200);
-	}
+        $content = $this->templating->render('MongoAdminBundle:Index:index.twig', array('collections' => $collections));
+        return new Response($content, 200);
+    }
 }
--- MongoManager.php
+++ MongoManager.php
@@ -6,39 +6,39 @@ use Symfony\Component\DependencyInjection\Container;
 
 class MongoManager {
 
-	const SERVICE_TAG = 'doctrine.odm.mongodb.document_manager';
-	
-	protected $container;
-	protected $documentManagers = array();
+    const SERVICE_TAG = 'doctrine.odm.mongodb.document_manager';
+    
+    protected $container;
+    protected $documentManagers = array();
 
-	public function __construct(container $container) {
-		$this->container = $container;
+    public function __construct(container $container) {
+        $this->container = $container;
 
-		foreach ($this->container->findTaggedServiceIds(static::SERVICE_TAG) as $serviceId => $tag) {
-			$this->documentManagers[$serviceId] = $this->container->get($serviceId);
-		}
-	}
+        foreach ($this->container->findTaggedServiceIds(static::SERVICE_TAG) as $serviceId => $tag) {
+            $this->documentManagers[$serviceId] = $this->container->get($serviceId);
+        }
+    }
 
-	public function getCollectionsArray() {
-		$collections = array();
+    public function getCollectionsArray() {
+        $collections = array();
 
-		foreach ($this->documentManagers as $serviceId => $documentManager) {
-			$collections[$serviceId] = array();
+        foreach ($this->documentManagers as $serviceId => $documentManager) {
+            $collections[$serviceId] = array();
 
-			$mongo = $documentManager->getMongo();
-			$databases = $mongo->listDBs();
+            $mongo = $documentManager->getMongo();
+            $databases = $mongo->listDBs();
 
-			foreach ($databases['databases'] as $database) {
-				$db = $mongo->selectDB($database['name']);
+            foreach ($databases['databases'] as $database) {
+                $db = $mongo->selectDB($database['name']);
 
-				$collections[$serviceId][$database['name']] = array();
+                $collections[$serviceId][$database['name']] = array();
 
-				foreach ($db->listCollections() as $collection) {
-					$collections[$serviceId][$database['name']][] = $collection->getName();
-				}
-			}
-		}
+                foreach ($db->listCollections() as $collection) {
+                    $collections[$serviceId][$database['name']][] = $collection->getName();
+                }
+            }
+        }
 
-		return $collections;
-	}
+        return $collections;
+    }
 }
