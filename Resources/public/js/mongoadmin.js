1ad3d046540fc36c6585ee5fa062b828cecc8ef9
Steven Surowiec <steven.surowiec@gmail.com>
2010-11-27 03:17:44 +0000

    Styling tweaks and removed collapsable tree for now

--- Resources/public/js/mongoadmin.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('#navigation-tree').treeview({
-        persist: "cookie",
-        cookieId: "mongoadmin-tree",
-        animated: "fast",
-        collapsed: true
-    });
-});
