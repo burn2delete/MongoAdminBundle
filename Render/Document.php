9bb0f58254d4dd9f9361c2431ae75d19c5bc6715
Steven Surowiec <steven.surowiec@gmail.com>
2010-11-28 18:31:01 +0000

    Display mongo ids as strings

--- Render/Document.php
+++ Render/Document.php
@@ -3,7 +3,19 @@
 namespace Bundle\MongoAdminBundle\Render;
 
 class Document {
-    public function preparePreview($document) {
-        return print_r($document, true);
+    public function preparePreview(array $document) {
+        $prepared = $this->prepareFields($document);
+
+        return print_r($prepared, true);
+    }
+
+    private function prepareFields(array $document) {
+        foreach ($document as $i => $value) {
+            if ($value instanceof \MongoId) {
+                $document[$i] = (string)$value;
+            }
+        }
+
+        return $document;
     }
 }
