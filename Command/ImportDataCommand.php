b8021f3b53a77b2031b15f4edcd30bda03a76f4c
Steven Surowiec <steven.surowiec@gmail.com>
2010-12-07 01:58:13 +0000

    First rough draft of import data command

---
+++ Command/ImportDataCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Bundle\MongoAdminBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportDataCommand extends Command {
+    public function configure() {
+        $this->setName('mongo-admin:import-data')
+            ->setDescription('Imports data from json files')
+            ->addArgument('server', InputArgument::REQUIRED, 'Server to import data to')
+            ->addArgument('database', InputArgument::REQUIRED, 'Database to import data to')
+            ->addArgument('file', InputArgument::REQUIRED, 'Path to json file');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output) {
+        $server = $input->getArgument('server');
+        $database = $input->getArgument('database');
+        $file = $input->getArgument('file');
+
+        if (!file_exists($file)) {
+            throw new \InvalidArgumentException(sprintf('Unable to locate file: %s', $file));
+        }
+
+        if (($mongo = $this->getMongoInstance($server)) === null) {
+            throw new \InvalidArgumentException(sprintf('Invalid server provided: %s', $server));
+        }
+
+        $encoded = file_get_contents($file);
+
+        if (($data = json_decode($encoded, true)) === null) {
+            throw new \InvalidArgumentException('Error decoding data');
+        }
+
+        $db = $this->getMongoDatabase($server, $database);
+        foreach ($data as $collectionName => $documents) {
+            $collection = $db->selectCollection($collectionName);
+
+            foreach ($documents as $document) {
+                $collection->insert($document);
+            }
+        }
+    }
+
+    protected function getMongoInstance($server) {
+        $manager = $this->container->get('mongo_admin.mongo_manager');
+        return $manager->getMongo($server);
+    }
+
+    protected function getMongoDatabase($server, $db) {
+        $manager = $this->container->get('mongo_admin.mongo_manager');
+        return $manager->getDatabase($server, $db);
+    }
+}
